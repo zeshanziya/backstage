2761d6a6998ce65fa71ad75384c8fea3a0c7eb75
zeshanziya <132985988+zeshanziya@users.noreply.github.com>
2024-01-04 03:11:05 +0000

    Deploying to gh-pages from @ zeshanziya/backstage@5ccf26657140f7fd141e0a483da5b001576c6e26 🚀

---
+++ assets/js/5e95c892.1e1d67cd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbackstage_microsite=self.webpackChunkbackstage_microsite||[]).push([[929661],{411768:(e,t,a)=>{a.r(t),a.d(t,{default:()=>k});var s=a(667294),r=a(434334),c=a(789712),u=a(565319),l=a(318790),n=a(533737);function k(e){return s.createElement(c.FG,{className:(0,r.Z)(u.k.wrapper.docsPages)},s.createElement(n.Z,null,(0,l.H)(e.route.routes)))}}}]);
